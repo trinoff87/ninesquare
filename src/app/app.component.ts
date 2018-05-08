ac4bda6b851ab71ccecf2f6888b3ba93abffda0c
Jose Fajardo <jose_fajardo@epam.com>
2018-05-08 18:04:16 -0500

    Added gmaps api support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Ninesquare';
+  a = 2;
+  b = 3;
+  ready = false;
+  name = '';
+  lat = 20.6118227;
+  lng = -103.4108922;
+  places: any = [
+    {active: true, name: 'Flowers House'},
+    {active: true, name: 'Kryspy Kreme'},
+    {active: false, name: 'Swagger Petstore'},
+  ];
+
+  constructor () {
+    setTimeout (() => {
+      this.ready = true;
+    }, 3000);
+  }
+
+  showMessage() {
+    alert('Hello');
+  }
 }
