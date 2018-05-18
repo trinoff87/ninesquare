845979cad1a363eff8433b40e2bc8a3ae3451b18
Jose Fajardo <jose_fajardo@epam.com>
2018-05-18 12:07:18 -0500

    Added material card and detail page

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 @Component({
   selector: 'app-detail',
@@ -8,11 +9,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailComponent implements OnInit {
 
+  places: any = [
+    {description: 'This is a flowers place', id: 1, pllatan: 'paid', nearness: 1, distance: 1, active: true, name: 'Flowers House'},
+    {description: 'This is a donut place', id: 2, plan: 'free', nearness: 2, distance: 5.3, active: true, name: 'Kryspy Kreme'},
+    {description: 'This is a pet place', id: 3, plan: 'paid', nearness: 3, distance: 8.7, active: false, name: 'Swagger Petstore'},
+  ];
+
+  id = null;
+  place = {};
+
   constructor(private route: ActivatedRoute) { 
     console.log(this.route.snapshot.params['id']);
+    console.log(this.route.queryParams);
+    console.log(this.route.snapshot.queryParams['action']);
+    console.log(this.route.snapshot.queryParams['referer']);
+    this.id = this.route.snapshot.params['id'];
+    console.log(this.findPlace());
+    this.place = this.findPlace();
   }
 
   ngOnInit() {
+  }
+
+  findPlace() {
+    return this.places.find(place =>
+      Number(place.id) === Number(this.id)
+    ) || null;
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule, MatListModule, MatDividerModule, MatButtonModule, MatIconModule} from '@angular/material';
+import {MatToolbarModule, MatListModule, MatDividerModule, MatButtonModule, MatIconModule, MatCardModule} from '@angular/material';
 
 const modules = [
   MatToolbarModule,
   MatListModule,
   MatDividerModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatCardModule
 ];
 
 @NgModule({
